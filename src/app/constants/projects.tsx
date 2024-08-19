3d590fd8616773a886a35600ea0ee851dc962106
JoePris <calpito.jp@gmail.com>
2024-08-18 19:36:20 -0600

    update image load

--- src/app/constants/projects.tsx
+++ src/app/constants/projects.tsx
@@ -14,7 +14,7 @@ export const projectItems: Array<ProjectItem> = [
         description: "Explore the source code snippets of this portfolio website, using TypeScript and NextJS for dynamic web development. The site is elegantly styled using Tailwind CSS and deployed on Vercel.",
         to: "project-item-0",
         repository: "https://github.com/joepris/my-portfolio",
-        images: ['village-life-pic1.png', 'village-life-pic2.png', 'village-life-pic3.png', 'village-life-pic4.png', 'village-life-pic5.png']
+        images: ['village-life-pic1.png', 'village-life-pic2.png', 'village-life-pic3.png', 'village-life-pic4.png', 'village-life-pic5.png','village-life-pic6.png','village-life-pic7.png','village-life-pic8.png']
     },
     {
         title: "This Portfolio Website",
