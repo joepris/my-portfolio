beefccc9b7dfdadbd08b60270c98773b040fda9b
JoePris <calpito.jp@gmail.com>
2024-08-18 19:58:19 -0600

    Updated work file

--- src/app/constants/work.tsx
+++ src/app/constants/work.tsx
@@ -9,19 +9,25 @@ export const workItems: Array<WorkItem> = [
         label: "Indie Game Developer",
         job_name: "Unity Game Developer",
         description: "Developed diverse apps using Unity, showcasing a range of skills and creativity in game development. Designed and implemented engaging user experiences with a focus on interactivity and functionality. Managed all aspects of development independently, demonstrating strong problem-solving abilities and attention to detail."
-    },{
+    },
+    {
         label: "Millenium Global Business Solutions - Bow Valley College Contract",
         job_name: "Junior Software Developer",
-        description: "Created a highly efficient and streamlined Hospital Scheduling App using ASP.NET MVC, designed to optimize scheduling and management processes within healthcare facilities. Leveraged expert proficiency in C# programming and the ASP.NET MVC framework to develop a robust and maintainable codebase."
+        description: "Developed an ASP.NET MVC Application, Hospital Scheduling App, designed for efficient streamline scheduling and management processes within hospitals. Utilized C# and the ASP.NET MVC framework to ensure efficient and maintainable codebase."
     },
     {
         label: "Pine Code",
         job_name: "Junior Software Developer",
-        description: "Effectively managed end-to-end development and continuous maintenance of websites, while successfully enhancing existing applications. Leveraged a comprehensive skill set encompassing HTML, CSS, JavaScript, ReactJS, NextJS, and TypeScript to meticulously create visually captivating and highly functional websites, aligning with precise design specifications and mockups."
+        description: "Managed the development and ongoing maintenance of websites while enhancing existing applications. Leveraged a diverse range of technologies including HTML, CSS, JavaScript, ReactJS, NextJS, and TypeScript to craft visually appealing and functional websites based on design specifications and mockups."
     },
     {
         label: "Rio Tuba Nickel Mining Corporation",
         job_name: "Mining Engineer Intern",
         description: "I enhanced my skills in optimization, efficiency, and data analysis, can be directly translated to software development. By applying these skills, I can contribute to creating high-performance software, improving workflows, and leveraging data-driven solutions by optimizing algorithms, data structures, and code execution, I can enhance the overall efficiency and speed of software applications."
+    },
+    {
+        label: "Calpito Law Office",
+        job_name: "Legal Assistant",
+        description: "As a legal assistant, I managed client interactions, document preparation, and administrative tasks. I welcomed clients professionally, creating a positive atmosphere. I handled paperwork with accuracy, ensuring adherence to legal guidelines. Additionally, I conducted legal research, assisted in preparing legal documents, organized tasks such as scheduling appointments and maintaining records, supported attorneys, and addressed client inquiries throughout the legal process."
     }
 ]
